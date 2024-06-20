b157237e66ffb616be79a772bf080d9ec48bf629
99duuk <99duuk@gmail.com>
2024-06-20 19:12:34 +0900

    feat: board entity 생성

--- src/main/java/com/luckyvicky/woosan/domain/board/entity/Board.java
+++ src/main/java/com/luckyvicky/woosan/domain/board/entity/Board.java
@@ -1,10 +1,47 @@
 package com.luckyvicky.woosan.domain.board.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.luckyvicky.woosan.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Board {
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private String postType;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime regDate;
+
+    @Column(nullable = false)
+    private int views;
+
+    @ColumnDefault("true")
+    private Boolean isDeleted;
+
+    @Column(nullable = false)
+    private String categoryName;
+
 }
