b287e4523cc04dfbc78f3b38cb6769f8973cdb82
hyeri-seo <hye_dam_2@naver.com>
2024-06-21 21:51:26 +0900

    feat: SpringSecurity를 위한 MemberType 생성

---
+++ src/main/java/com/luckyvicky/woosan/domain/member/entity/MemberType.java
@@ -0,0 +1,24 @@
+package com.luckyvicky.woosan.domain.member.entity;
+
+public enum MemberType {
+    USER(Level.class), GUEST, ADMIN;
+
+    // 레벨 클래스를 저장하기 위한 필드 (USER에만 적용됨)
+    private Class<? extends Enum<?>> levelCLass;
+
+    MemberType() {
+        this.levelCLass = null;
+    }
+
+    MemberType(Class<? extends Enum<?>> levelCLass) {
+        this.levelCLass = levelCLass;   // levelClass 초기화
+    }
+
+    public Class<? extends Enum<?>> getLevelCLass() {
+        return levelCLass;  // 연관된 레벨 클래스 반환
+    }
+
+    public enum Level {
+        LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, LEVEL_5;
+    }
+}
