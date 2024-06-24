9a67d857e3738f08694eae0201fb541328fa22dc
Hoje0715 <ho9760@naver.com>
2024-06-24 13:53:46 +0900

    feat: Report Entity 생성 및 JPA CRUD 테스트

--- src/main/java/com/luckyvicky/woosan/domain/report/mapper/ReportMapper.java
+++
@@ -1,4 +0,0 @@
-package com.luckyvicky.woosan.domain.report.mapper;
-
-public class ReportMapper {
-}
--- src/main/java/com/luckyvicky/woosan/domain/report/service/ReportServiceImpl.java
+++ src/main/java/com/luckyvicky/woosan/domain/report/service/ReportServiceImpl.java
@@ -1,4 +1,25 @@
 package com.luckyvicky.woosan.domain.report.service;
 
+import com.luckyvicky.woosan.domain.fileImg.repository.FileImgRepository;
+import com.luckyvicky.woosan.domain.member.entity.Member;
+import com.luckyvicky.woosan.domain.member.repository.MemberRepository;
+import com.luckyvicky.woosan.domain.report.repository.ReportRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class ReportServiceImpl implements ReportService {
+
+    private final ReportRepository reportRepository;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void reportTarget(Long reporterId, String type, Long targetId) {
+        Member reporter = memberRepository.findById(reporterId).get();
+
+
+    }
 }
