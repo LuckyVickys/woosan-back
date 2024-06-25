9e0314b5842164bb8a298dcb1030ea25dd8a3e31
hyeri-seo <141993427+hyeri-seo@users.noreply.github.com>
2024-06-25 17:12:46 +0900

    feat: 임시 비밀번호 생성 코드

--- src/main/java/com/luckyvicky/woosan/domain/member/service/MemberService.java
+++ src/main/java/com/luckyvicky/woosan/domain/member/service/MemberService.java
@@ -1,9 +1,19 @@
 package com.luckyvicky.woosan.domain.member.service;
 
+import com.luckyvicky.woosan.domain.member.dto.MailDTO;
 import com.luckyvicky.woosan.domain.member.entity.Member;
 
 public interface MemberService {
     Boolean existEmail(String email) throws Exception;
     Boolean existNickname(String nickname) throws Exception;
     Member addMember(Member member) throws Exception;
+
+    /**
+     * 임시 비밀번호 발급 및 비밀번호 변경 관련 코드들
+     */
+//    MailDTO createMailAndChangePw(String email) throws Exception;
+//    void updateTempPw(String str, String email) throws Exception;
+    String getTempPassword() throws Exception;
+//    void mailSend(MailDTO mailDTO) throws Exception;
+//    void updatePassword(String email, String newPassword) throws Exception;
 }
--- src/main/java/com/luckyvicky/woosan/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/luckyvicky/woosan/domain/member/service/MemberServiceImpl.java
@@ -1,12 +1,17 @@
 package com.luckyvicky.woosan.domain.member.service;
 
+import com.luckyvicky.woosan.domain.member.dto.MailDTO;
 import com.luckyvicky.woosan.domain.member.entity.Member;
 import com.luckyvicky.woosan.domain.member.entity.MemberType;
 import com.luckyvicky.woosan.domain.member.entity.SocialType;
 import com.luckyvicky.woosan.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +19,7 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 //    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JavaMailSender mailSender;
 
     // 이메일 중복 체크
     @Override
@@ -48,5 +54,77 @@ public class MemberServiceImpl implements MemberService {
 
         return memberRepository.save(member);
     }
+
+    /**
+     * 임시 비밀번호 발급 및 비밀번호 변경 관련 코드들
+     */
+    // 메일 내용 생성 및 임시 비밀번호로 회원 비밀번호 변경
+//    @Override
+//    public MailDTO createMailAndChangePw(String email) throws Exception {
+//        String str = getTempPassword();
+//        MailDTO dto = new MailDTO(email,
+//                "Woosan 임시비밀번호 안내 이메일입니다.",
+//                "안녕하세요. Woosan 임시비밀번호 안내 관련 이메일 입니다." + " 회원님의 임시 비밀번호는 "
+//                        + str + " 입니다." + "로그인 후에 비밀번호를 변경을 해주세요");
+//        updateTempPw(str,email);
+//        return dto;
+//    }
+//
+//    // 임시 비밀번호로 업데이트
+//    @Override
+//    public void updateTempPw(String str, String email) throws Exception {
+//        Member member = memberRepository.findByEmail(email);
+//        if(member != null) {
+//            member.changePassword(str);
+////        member.changePassword(bCryptPasswordEncoder.encode(str));
+//            memberRepository.save(member);
+//        } else {
+//            throw new Exception("회원정보가 일치하지 않습니다.");
+//        }
+//    }
+
+    // 랜덤함수로 임시 비밀번호 구문 만들기
+    @Override
+    public String getTempPassword() throws Exception {
+        char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        String str = "";
+
+        // 문자 배열 길이의 값을 랜덤으로 10개를 뽑아 구문을 작성함
+        int idx = 0;
+        for (int i = 0; i < 10; i++) {
+            idx = (int) (charSet.length * Math.random());
+            str += charSet[idx];
+        }
+        return str;
+    }
+
+    // 메일 전송
+//    @Override
+//    public void mailSend(MailDTO mailDTO) throws Exception {
+//        System.out.println("메일 전송 완료");
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(mailDTO.getEmail());
+//        message.setSubject(mailDTO.getTitle());
+//        message.setText(mailDTO.getMessage());
+//        message.setFrom("보낸이@naver.com");
+//        message.setReplyTo("보낸이@naver.com");
+//        System.out.println("message" + message);
+//        mailSender.send(message);
+//    }
+//
+//    // 비밀번호 변경
+//    @Override
+//    public void updatePassword(String email, String newPassword) throws Exception {
+//        Member member = memberRepository.findByEmail(email);
+//        if(member != null) {
+//            member.changePassword(newPassword);
+////        member.changePassword(bCryptPasswordEncoder.encode(newPassword));
+//            memberRepository.save(member);
+//        } else {
+//            throw new Exception("회원정보가 일치하지 않습니다.");
+//        }
+//    }
 
 }
