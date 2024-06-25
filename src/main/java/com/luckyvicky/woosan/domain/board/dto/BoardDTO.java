239432378196df8ae55aba9f7dcec589b9da9d04
99duuk <99duuk@gmail.com>
2024-06-25 10:04:02 +0900

    feat/  DTO 이미지 관련 필드 추가

--- src/main/java/com/luckyvicky/woosan/domain/board/dto/BoardDTO.java
+++ src/main/java/com/luckyvicky/woosan/domain/board/dto/BoardDTO.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -16,12 +18,15 @@ public class BoardDTO {
 
     private Long id;
     private MemberDTO writer;
-//    private int postType;
     private String title;
     private String content;
     private LocalDateTime regDate;
     private int views;
     private int likesCount;
     private String categoryName;
+
+    private List<MultipartFile> images;
+    private List<String> filePathUrl;
+
 
 }
