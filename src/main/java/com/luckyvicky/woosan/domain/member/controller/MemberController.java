1e453e06f0e4a89227ec6f8849af04546c8c9465
hyeri-seo <141993427+hyeri-seo@users.noreply.github.com>
2024-06-25 16:29:36 +0900

    fix: MemberController

--- src/main/java/com/luckyvicky/woosan/domain/member/controller/MemberController.java
+++ src/main/java/com/luckyvicky/woosan/domain/member/controller/MemberController.java
@@ -1,16 +1,20 @@
 package com.luckyvicky.woosan.domain.member.controller;
 
+import com.luckyvicky.woosan.domain.member.dto.MailDTO;
 import com.luckyvicky.woosan.domain.member.dto.SignUpReqDTO;
 import com.luckyvicky.woosan.domain.member.dto.SignUpResDTO;
+import com.luckyvicky.woosan.domain.member.dto.UpdatePwDTO;
 import com.luckyvicky.woosan.domain.member.entity.Member;
 import com.luckyvicky.woosan.domain.member.mapper.MemberMapper;
 import com.luckyvicky.woosan.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MemberController {
 
@@ -18,7 +22,7 @@ public class MemberController {
     private final MemberService memberService;
 
     // 이메일 중복 체크
-    @GetMapping("email/{email}")
+    @GetMapping("/email/{email}")
     public ResponseEntity<Object> emailCheck(@PathVariable String email) {
         try {
             return new ResponseEntity(memberService.existEmail(email), HttpStatus.OK);
@@ -29,7 +33,7 @@ public class MemberController {
     }
 
     // 닉네임 중복 체크
-    @GetMapping("nickname/{nickname}")
+    @GetMapping("/nickname/{nickname}")
     public ResponseEntity<Object> nicknameCheck(@PathVariable String nickname) {
         try {
             return new ResponseEntity(memberService.existNickname(nickname), HttpStatus.OK);
@@ -47,6 +51,42 @@ public class MemberController {
             member = memberService.addMember(member);
             SignUpResDTO memberRes = mapper.memberToSignUpResDTO(member);
             return new ResponseEntity<>(memberRes, HttpStatus.CREATED);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Transactional
+    @PostMapping("/sendEmail")
+    public ResponseEntity<Object> sendEmail(@RequestParam("email") String email) {
+        try {
+            MailDTO dto = memberService.createMailAndChangePw(email);
+            memberService.mailSend(dto);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // 임시 비밀번호 발급
+    @PutMapping("/updateTempPw/{email}")
+    public ResponseEntity<Object> updateTempPw(@PathVariable String email) {
+        try {
+            memberService.createMailAndChangePw(email);
+            return new ResponseEntity<>(true, HttpStatus.CREATED);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/updatePw")
+    public ResponseEntity<Object> updatePw(@RequestBody UpdatePwDTO updatePwDTO) {
+        try {
+            memberService.updatePassword(updatePwDTO.getEmail(), updatePwDTO.getPassword());
+            return new ResponseEntity<>(true, HttpStatus.CREATED);
         } catch(Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
