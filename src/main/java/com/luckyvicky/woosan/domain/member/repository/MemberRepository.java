f5cabece81cdae3cd965a3f0dcc0f4e85608a773
coding-salmon <73780359+coding-salmon@users.noreply.github.com>
2024-06-27 10:32:01 +0900

    fix : 멤버타입 수정 , 멤버리포지토리 임포트 수정

--- src/main/java/com/luckyvicky/woosan/domain/member/repository/MemberRepository.java
+++ src/main/java/com/luckyvicky/woosan/domain/member/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.luckyvicky.woosan.domain.member.repository;
 
 import com.luckyvicky.woosan.domain.member.entity.Member;
-import com.luckyvicky.woosan.domain.member.entity.MemberProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
