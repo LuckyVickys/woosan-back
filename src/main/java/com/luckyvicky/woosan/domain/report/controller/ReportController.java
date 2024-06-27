a53266a8a3b5715181d584ed2e9b1af592e8855e
Hoje0715 <ho9760@naver.com>
2024-06-27 17:13:23 +0900

    fix: 신고 api 수정

--- src/main/java/com/luckyvicky/woosan/domain/report/controller/ReportController.java
+++ src/main/java/com/luckyvicky/woosan/domain/report/controller/ReportController.java
@@ -3,6 +3,7 @@ package com.luckyvicky.woosan.domain.report.controller;
 import com.luckyvicky.woosan.domain.report.dto.ReportDTO;
 import com.luckyvicky.woosan.domain.report.service.ReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,8 +18,16 @@ public class ReportController {
     private final ReportService reportService;
 
     @PostMapping("/add")
-    public ResponseEntity<Long> reportAdd(@RequestBody ReportDTO reportDTO) {
-        Long reportId = reportService.reportAdd(reportDTO);
-        return ResponseEntity.ok(reportId);
+    public ResponseEntity<Object> reportAdd(@RequestBody ReportDTO reportDTO) {
+
+//        Long reportId = reportService.reportAdd(reportDTO);
+//        return ResponseEntity.ok(reportId);
+        try {
+            ReportDTO reportAddDTO = reportService.reportAdd(reportDTO);
+            return new ResponseEntity<>(reportAddDTO, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/com/luckyvicky/woosan/domain/report/repository/ReportRepository.java
+++ src/main/java/com/luckyvicky/woosan/domain/report/repository/ReportRepository.java
@@ -1,7 +1,13 @@
 package com.luckyvicky.woosan.domain.report.repository;
 
+import com.luckyvicky.woosan.domain.member.entity.Member;
 import com.luckyvicky.woosan.domain.report.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    List<Report> findByReporterAndTypeAndTargetId(Member reporter, String type, Long targetId);
 }
