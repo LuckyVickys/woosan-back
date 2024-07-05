452d71d32734a039a64c8b5489c49b9c51cde5d6
hyeri-seo <141993427+hyeri-seo@users.noreply.github.com>
2024-07-05 10:18:25 +0900

    feat: clova summary 이용해서 게시글 상세 페이지 세 줄 요약 기능 구현

--- src/main/java/com/luckyvicky/woosan/domain/board/controller/BoardController.java
+++ src/main/java/com/luckyvicky/woosan/domain/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.luckyvicky.woosan.domain.board.controller;
 
 import com.luckyvicky.woosan.domain.board.dto.BoardDTO;
 import com.luckyvicky.woosan.domain.board.dto.BoardPageResponseDTO;
+import com.luckyvicky.woosan.domain.board.service.SummaryService;
 import com.luckyvicky.woosan.global.util.PageRequestDTO;
 import com.luckyvicky.woosan.domain.board.service.BoardService;
 import com.luckyvicky.woosan.domain.board.service.PapagoService;
@@ -21,7 +22,7 @@ public class BoardController {
 
     private final BoardService boardService;
     private final PapagoService papagoService;
-
+    private final SummaryService summaryService;
 
     /**
      * 게시물 작성
@@ -121,5 +122,24 @@ public class BoardController {
         }
     }
 
+    /**
+     * 게시물 요약
+     */
+    @PostMapping("/{id}/summary")
+    public ResponseEntity<String> boardDetailSummary(@PathVariable("id") Long id, @RequestBody BoardDTO boardDTO) {
+        System.out.println("==========================================");
+        System.out.println("요약 기능");
+        System.out.println(boardDTO);
+        System.out.println("==========================================");
 
+        String summary = "";
+
+        try {
+            summary = summaryService.summaryBoardDetailPage(boardDTO);
+            System.out.println(summary);
+            return ResponseEntity.ok(summary);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
