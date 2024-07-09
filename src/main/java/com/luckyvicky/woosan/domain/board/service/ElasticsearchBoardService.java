6606c4b4cf6de46fccd375ef36892fc0edfdc396
99duuk <99duuk@gmail.com>
2024-07-09 17:05:06 +0900

    feat: 검색기능 1차 구현

--- src/main/java/com/luckyvicky/woosan/domain/board/service/ElasticsearchBoardService.java
+++ src/main/java/com/luckyvicky/woosan/domain/board/service/ElasticsearchBoardService.java
@@ -1,10 +1,12 @@
 package com.luckyvicky.woosan.domain.board.service;
 
+import com.luckyvicky.woosan.domain.board.dto.BoardPageResponseDTO;
 import com.luckyvicky.woosan.domain.board.entity.Board;
+import com.luckyvicky.woosan.global.util.PageRequestDTO;
 
 import java.util.List;
 
 public interface ElasticsearchBoardService {
 
-    List<Board> searchByCategoryAndFilter(String categoryName, String filterType, String keyword);
+    BoardPageResponseDTO searchByCategoryAndFilter(String categoryName, String filterType, String keyword, PageRequestDTO pageRequestDTO);
 }
